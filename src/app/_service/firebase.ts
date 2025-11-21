76698c7019ec903c6d70063cde8115dd2fe81a9a
FilipaBarreto <replayandreplay@gmail.com>
2025-11-21 21:34:45 +0000

    Apdate da key

--- src/app/_service/firebase.ts
+++ src/app/_service/firebase.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+// Importe outros serviços conforme necessário
+
+import { Injectable } from "@angular/core";
+import { key } from "./key";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class Firebase {
-  
+  private app;
+  public auth;
+  public firestore;
+  public storage;
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: key,
+      authDomain: "ebook-exemplo2.firebaseapp.com",
+      projectId: "ebook-exemplo2",
+      storageBucket: "ebook-exemplo2.firebasestorage.app",
+      messagingSenderId: "407843143200",
+      appId: "1:407843143200:web:775508a895c49f02bf8645"
+    };
+    // Initialize Firebase
+    this.app = initializeApp(firebaseConfig);
+    this.auth = getAuth(this.app);
+    this.firestore = getFirestore(this.app);
+    this.storage = getStorage(this.app);
+  }
 }
---
+++ src/app/_service/key.ts
@@ -0,0 +1,1 @@
+export const key = "AIzaSyCgkyxzDPAK9UTO49coBx17Zcjy2uXnX-0";
